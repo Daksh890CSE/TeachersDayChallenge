9e14debef5747853d2708a969f94c4cfc56da9d5
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-27 15:25:33 +0530

    Some changes in Plaindrom Code

--- ValidPlaindrome/src/App.java
+++ ValidPlaindrome/src/App.java
@@ -2,12 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        String regex="[^A-Za-z]";
+        s=s.replaceAll(regex,"");
+        s=s.toLowerCase();
+        for(int i=0;s.length())
+        return true;
     }
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner kb= new Scanner(System.in);
+        System.out.println("Enter String to verify palindrome: ");
+        String s=kb.nextLine();
+        Solution obj = new Solution();
+        boolean check=obj.isPalindrome(s);
     }
 }
