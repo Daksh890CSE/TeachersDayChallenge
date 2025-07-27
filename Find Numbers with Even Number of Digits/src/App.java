7631ea5d1d5e3b7032190edbe17662770e69e81f
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-27 21:58:47 +0530

    Implement input handling in App class to read array size and elements for processing

--- Find Numbers with Even Number of Digits/src/App.java
+++ Find Numbers with Even Number of Digits/src/App.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public int findNumbers(int[] nums) {
         
@@ -5,7 +7,17 @@ class Solution {
 }
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n=kb.nextInt();
+        int[] nums=new int[n];
+        System.out.println("Enter elements of array");
+        for(int i=0;i<n;i++){
+            nums[i]=kb.nextInt();
+        }
+        Solution obj=new Solution();
+        int result=obj.findNumbers(nums);
+        System.out.println(result);
     }
 }
