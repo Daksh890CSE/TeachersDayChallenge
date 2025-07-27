4e2d567a0f23fd4899fceb22885b2d8449de6f82
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-27 21:46:26 +0530

    Initialize Java project structure with source, output, and dependencies; add basic App and Solution classes

---
+++ Find Numbers with Even Number of Digits/src/App.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
