547a0d0d75463d7911824a376060e60059d3a116
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-28 15:09:05 +0530

    Add initial project structure with App and Solution classes, and configuration settings

---
+++ Fizz Buzz/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+       
+  
+    }
+      public static void main(String[] args){
+         Scanner kb=new Scanner(System.in);
+         System.out.println("Enter the integer");
+         int n=kb.nextInt();
+         Solution obj=new Solution();
+         obj.fizzBuzz(n);
+    }
+}
