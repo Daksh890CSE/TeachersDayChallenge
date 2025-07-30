af3756039937b0089a52998db5fe66cf206c91f3
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-31 00:33:42 +0530

    Refactor maxProfit method to correct logic for calculating purchase and sell days, improving stock profit calculation

--- Best Time to Buy and Sell Stock/src/Solution.java
+++ Best Time to Buy and Sell Stock/src/Solution.java
@@ -2,40 +2,49 @@ import java.util.*;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int len=prices.length;
-        int purchase=prices[0];
-        int sell=prices[len];
-        int day=len;
-        for(int i=1;i<len;i++){
-            if(prices[i-1]<prices[i]){
-                if(purchase<=prices[i-1]){
-                    purchase=prices[0];
-                }else{
-                    purchase=prices[i-1];
+        int len = prices.length;
+        int purchase = prices[0];
+        int sell = prices[len - 1];
+        int purchase_day = 1;
+        int sell_day = len - 1;
+        for (int i = 1; i < len; i++) {
+            if (prices[i - 1] < prices[i]) {
+                if (purchase >= prices[i - 1]) {
+                    purchase = prices[i - 1];
+                    purchase_day = i + 1;
                 }
-            }else{
-                if(sell<prices[i-1]){
-                    sell=prices[i-1];
-                    day=i-1;
-                }else{
-                    sell=prices[len];
+            } else if (purchase == prices[0]) {
+                if (prices[i - 1] > prices[i]) {
+                    purchase_day = len;
+                } else {
+                    purchase_day=1;
                 }
-            }if(sell==purchase){
-                return day=0;
             }
         }
-        return day;
-    }
-    public static void main(String args[]){
-        Scanner kb=new Scanner(System.in);
-        System.out.println("Enter size of the array: ");
-        int n=kb.nextInt();
-        int[] prices=new int[n];
-        System.out.println("Enter the elements of the array: ");
-        for(int i=0;i<n;i++){
-            prices[i]=kb.nextInt();
+        if (purchase_day < len) {
+            for (int j = purchase_day; j < len; j++) {
+                if (sell < prices[j]) {
+                    sell = prices[j];
+                    sell_day = j + 1;
+                }
+            }
+        } else {
+            sell_day = 0;
         }
-        Solution obj=new Solution();
+        return sell_day;
+    }
+
+    public static void main(String args[]) {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter size of the array: ");
+        int n = kb.nextInt();
+        int[] prices = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            prices[i] = kb.nextInt();
+        }
+        Solution obj = new Solution();
         System.out.println(obj.maxProfit(prices));
+        kb.close();
     }
 }
