5c51af6a9fbd13b18f8fd7b1e8000e940c168659
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-30 18:55:29 +0530

    Add initial project structure with settings, main app, and solution class for Roman to Integer conversion

---
+++ Roman to Integer/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        Hashtable<String,Integer> ht=new Hashtable<>();
+        ht.put("I", 1);
+        ht.put("V", 5);
+        ht.put("X", 10);
+        ht.put("L", 50);
+        ht.put("L", 50);
+        ht.put("c", 100);
+        ht.put("D", 500);
+        ht.put("M", 1000);
+        int total=0;
+    }
+    public static void main(String args[]){
+        Scanner kb=new Scanner(System.in);
+        
+    }
+}
