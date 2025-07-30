073a7d747d1f6e5a516f25dfebed28999cb3f278
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-07-30 22:41:39 +0530

    Add initial implementation of Solution class for stock profit calculation and project settings

---
+++ Best Time to Buy and Sell Stock/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        for(int i=0;i>=prices.length-1;i--){
+            if(){
+
+            }
+        }
+    }
+    public static void main(String args[]){
+        Scanner kb=new Scanner(System.in);
+        System.out.println("Enter size of the array: ");
+        int n=kb.nextInt();
+        int[] prices=new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            prices[i]=kb.nextInt();
+        }
+    }
+}
