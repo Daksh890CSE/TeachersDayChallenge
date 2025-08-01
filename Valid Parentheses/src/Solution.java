749c6cd1ba8d25b5867fa719880689564650a83c
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-01 22:23:03 +0530

    Refactor isValid method to correct stack empty check for improved logic in parentheses validation

--- Valid Parentheses/src/Solution.java
+++ Valid Parentheses/src/Solution.java
@@ -6,7 +6,7 @@ class Solution {
         if (s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}') {
             return false;
         }
-        if (stk.empty() == false) {
+        if (!stk.isEmpty()) {
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
                     stk.push(s.charAt(i));
