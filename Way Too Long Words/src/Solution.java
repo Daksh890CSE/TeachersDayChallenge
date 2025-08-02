5131f5d709d4cc1b1bfc801b665467c9579a3604
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-02 21:07:27 +0530

    Add initial implementation of Solution class and project configuration files

---
+++ Way Too Long Words/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public String WayTwoLong(String s){
+        int len=s.length();
+        if(len<10){
+            return s;
+        }else{
+            char start=s.charAt(0);
+            char end=s.charAt(len-1);
+            int num=len-2;
+            s=String.valueOf(start)+num+String.valueOf(end);
+        }
+        return s;
+    }
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        Solution obj=new Solution();
+        int n=kb.nextInt();
+        for(int i=0;i<n;i++){
+            String s=kb.next();
+            System.out.println(obj.WayTwoLong(s));
+        }
+        kb.close();
+    }
+}
