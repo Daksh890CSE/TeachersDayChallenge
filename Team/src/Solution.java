0f058e963d2ebae326ba63e689ebed40dbae23f2
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-03 19:18:01 +0530

    Remove Team method and related logic from Solution class; refactor main method for clarity

--- Team/src/Solution.java
+++ Team/src/Solution.java
@@ -1,24 +1,11 @@
 import java.util.*;
 
 public class Solution {
-    public int Team(int[] arr){
-        int count=0;
-        int solved=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==1){
-                count+=1;
-                if(count>=2){
-                    solved+=1;
-                }
-            }
-        }
-        return solved;
-    }
-    
-    public static void main(String[] args){
+   public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
-        Solution obj=new Solution();
         int n=kb.nextInt();
+        int count=0;
+        int solved =0;
         for(int i=0;i<n;i++){
             String str=kb.nextLine();
             String[] numstr= str.split(" ");
@@ -27,6 +14,5 @@ public class Solution {
                 num[j]=Integer.parseInt(numstr[j]);
             }
         }
-        System.out.println(obj.Team(num));
     }
 }
