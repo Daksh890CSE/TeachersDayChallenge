7d6ac160583270472efe6a6f13ad75337b60c2e3
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-03 19:18:21 +0530

    Refactor count variable placement in Solution class to ensure correct logic in main method

--- Team/src/Solution.java
+++ Team/src/Solution.java
@@ -4,7 +4,6 @@ public class Solution {
    public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        int count=0;
         int solved =0;
         for(int i=0;i<n;i++){
             String str=kb.nextLine();
@@ -12,6 +11,10 @@ public class Solution {
             int[] num= new int[numstr.length]; 
             for(int j=0;j<numstr.length;j++){
                 num[j]=Integer.parseInt(numstr[j]);
+                int count=0;
+                if(count>=2){
+                    solved+=1;
+                }
             }
         }
     }
