7cb3de7e59e84605c091b744246998b76d5aaaac
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-04 20:36:31 +0530

    Implement string processing logic to remove vowels and print the modified string in lowercase.

--- String Task/src/Solution.java
+++ String Task/src/Solution.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args){
-            String regex=[]
+        Scanner kb =new Scanner(System.in);
+        String s=kb.next();
+        String regex="[aeiouAEIOU]";
+        s=s.toLowerCase();
+        s=s.replaceAll(regex,"");
+        System.out.println(s);
     }
 
 }
