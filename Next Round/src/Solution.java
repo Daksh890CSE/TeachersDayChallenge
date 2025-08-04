b86b7501c1a0af100ae3798b2ee0fea168540b53
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-04 15:16:39 +0530

    Add kb.close() to the Solution class's main method to close the scanner and prevent resource leaks.

--- Next Round/src/Solution.java
+++ Next Round/src/Solution.java
@@ -34,5 +34,6 @@ public class Solution {
             
         }
         System.out.println(count);
+        kb.close();
     }
 }
---
+++ Rotate Array/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        arr
+    }
+    public static void mina(String[] args){
+        Scanner kb=new Scanner(System.in);
+    }
+}
