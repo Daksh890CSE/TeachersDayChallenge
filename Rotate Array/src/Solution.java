7ed43f2ab388ddfc8b6b239c257ece819c0548fa
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-04 15:34:41 +0530

    Implement the rotate method in Solution class to rotate an array by k positions.

--- Rotate Array/src/Solution.java
+++ Rotate Array/src/Solution.java
@@ -3,7 +3,20 @@ import java.util.*;
 
 class Solution {
     public void rotate(int[] nums, int k) {
-        arr
+        int len=nums.length;
+        int[] rotated=new int[k];
+        int[] normal=new int[len-k];
+        int[] rotated_arr=new int[len];
+        for(int i=len-k;i<len;i++){
+            for(int j=0;j<k;j++){
+                rotated[j]=nums[i];
+            }
+        }
+        for(int i=0;i<len-k;i++){
+            normal[i]=nums[i];
+        }
+        System.arraycopy(rotated, 0, rotated_arr, 0, rotated.length);
+        System.arraycopy(normal, 0, rotated_arr, rotated.length, normal.length);
     }
     public static void mina(String[] args){
         Scanner kb=new Scanner(System.in);
