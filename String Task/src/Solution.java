e1b25d80585b540db5672d15aa4a9b123b44db0b
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-04 20:50:52 +0530

    Modify the regex in main method to include 'y' and add dots before each character in the string.

--- String Task/src/Solution.java
+++ String Task/src/Solution.java
@@ -4,10 +4,18 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb =new Scanner(System.in);
         String s=kb.next();
-        String regex="[aeiouAEIOU]";
+        String regex="[aeiouyAEIOUY]";
         s=s.toLowerCase();
         s=s.replaceAll(regex,"");
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<s.length()*2;i++){
+            if(i==0 || i%2==0){
+                sb.insert(i,'.');
+            }
+        }
+        s=sb.toString();
         System.out.println(s);
+        kb.close();
     }
 
 }
