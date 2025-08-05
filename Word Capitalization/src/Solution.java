1f06576fdb002bce099562a7e0301d473bc9f214
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-05 22:02:52 +0530

    Add initial files including Solution.java, settings.json, and README.md to set up the Java project structure.

---
+++ Word Capitalization/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+        public static void main(String[] args){
+            Scanner kb=new Scanner(System.in);
+            String s=kb.nextLine();
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)==s.t
+            }
+        }
+}
