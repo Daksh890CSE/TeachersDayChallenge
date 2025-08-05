e01cd69b7b08334574bb5799ed761736b6c914ab
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-05 23:04:48 +0530

    Capitalize the first letter of the input string in the main method.

--- Word Capitalization/src/Solution.java
+++ Word Capitalization/src/Solution.java
@@ -4,8 +4,10 @@ public class Solution {
         public static void main(String[] args){
             Scanner kb=new Scanner(System.in);
             String s=kb.nextLine();
-            for(int i=0;i<s.length();i++){
-                if(s.charAt(i)==s.t
+            if(Character.isLowerCase(s.charAt(0))){
+                s=s.replace(String.valueOf(s.charAt(0)),String.valueOf(Character.toUpperCase(s.charAt(0))));
             }
+            System.out.println(s);
+            kb.close();
         }
 }
