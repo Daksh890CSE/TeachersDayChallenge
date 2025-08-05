2a524940bf68425955d6ad8896fc2fb86217b8ec
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-05 23:09:20 +0530

    Refactor main method in Solution class to use replaceFirst for capitalizing the first letter of the input string.

--- Word Capitalization/src/Solution.java
+++ Word Capitalization/src/Solution.java
@@ -4,9 +4,7 @@ public class Solution {
         public static void main(String[] args){
             Scanner kb=new Scanner(System.in);
             String s=kb.nextLine();
-            if(Character.isLowerCase(s.charAt(0))){
-                s=s.replace(String.valueOf(s.charAt(0)),String.valueOf(Character.toUpperCase(s.charAt(0))));
-            }
+            s=s.replaceFirst(String.valueOf(s.charAt(0)),String.valueOf(Character.toUpperCase(s.charAt(0))));
             System.out.println(s);
             kb.close();
         }
