e321e231f510507d99b8a7abfdeb389da5ec6bfe
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-05 20:37:01 +0530

    Refactor main method in Solution class to correctly sort and concatenate numbers from input string.

--- Helpful Maths/src/Solution.java
+++ Helpful Maths/src/Solution.java
@@ -4,17 +4,22 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
+        String fnl="";
         try{
         if(s.length()==1){
             System.out.println(s);
         }
-        String[] str=s.split("+");
+        }catch(Exception e){}
+        String[] str=s.split("\\+");
         int[] num=new int[str.length];; 
         for(int i=0;i<str.length;i++){
             num[i]=Integer.parseInt(str[i]);
         }
         Arrays.sort(num);
-        for
-        }catch(Exception e){}
+        for(int i=0;i<num.length;i++){
+            fnl=fnl+String.valueOf(num[i]);
+        }
+        System.out.println(fnl);
+        
     }
 }
