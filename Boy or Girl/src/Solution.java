6aeddd441a30669d5e2d2506b2914b86c45a203b
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-06 11:45:47 +0530

    Implement character counting and string manipulation logic in the main method.

--- Boy or Girl/src/Solution.java
+++ Boy or Girl/src/Solution.java
@@ -3,6 +3,16 @@ import java.util.*;
 public class Solution{
     public static void main(String args[]){
         Scanner kb=new Scanner(System.in);
-        
+        String s=kb.nextLine();
+        String str;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            s=s.replaceAll(String.valueOf(s.charAt(i)), "");
+            System.out.println(s);
+            count+=1;
+            if(s.isEmpty()){
+                break;
+            }
+        }System.out.println(count);
     }
 }
