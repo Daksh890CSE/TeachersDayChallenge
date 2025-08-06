63b4ebafccc96f0af5ab8f0c4fb1ebf453a7544e
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-06 15:49:46 +0530

    Refactor the loop condition in the Solution class to use isBlank() instead of isEmpty() to handle whitespace correctly.

--- Boy or Girl/src/Solution.java
+++ Boy or Girl/src/Solution.java
@@ -6,11 +6,13 @@ public class Solution{
         String s=kb.nextLine();
         String str;
         int count=0;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;!s.isBlank();i++){
             s=s.replaceAll(String.valueOf(s.charAt(i)), "");
             System.out.println(s);
+            int len=s.length();
+            System.out.println(len);
             count+=1;
-            if(s.isEmpty()){
+            if(s.isBlank()){
                 break;
             }
         }System.out.println(count);
