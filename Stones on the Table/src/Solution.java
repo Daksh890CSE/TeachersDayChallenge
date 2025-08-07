b60ddc19ba96c893e8a1d1a395aeaea030fdc1c4
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-07 19:11:35 +0530

    Add initial Java code structure with Solution class and main method.

---
+++ Stones on the Table/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String s = kb.nextLine();
+    }
+}
