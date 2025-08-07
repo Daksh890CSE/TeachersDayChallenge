3c5890d329bd7e02e08ca764fd869b75a8bfd510
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-07 20:12:07 +0530

    Implement string comparison to check if reversed input matches second input in Solution class

--- Translation/src/Solution.java
+++ Translation/src/Solution.java
@@ -4,9 +4,14 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
+        String s1=kb.nextLine();
         StringBuilder sb=new StringBuilder(s);
         sb.reverse();
         String rev=sb.toString();
-        
+        if(rev==s1){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
