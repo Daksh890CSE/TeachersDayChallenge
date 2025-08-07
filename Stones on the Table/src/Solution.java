d5a953340349f0758bda809898eb962fa34445de
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-07 19:26:04 +0530

    Implement logic to count consecutive identical characters in the main method of Solution class.

--- Stones on the Table/src/Solution.java
+++ Stones on the Table/src/Solution.java
@@ -4,6 +4,15 @@ public class Solution {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
+        kb.nextLine();
         String s = kb.nextLine();
+        int count=0;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i-1)==s.charAt(i)){
+                if(count<s.length()-1){
+
+                }
+            }
+        }
     }
 }
