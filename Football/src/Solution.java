e8119acc91a416d6a61c48c4a92c3ac2e0e3571d
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-12 21:57:18 +0530

    Implement main logic in Solution class to check for consecutive zeros or ones

--- Football/src/Solution.java
+++ Football/src/Solution.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args){
-        
+        Scanner kb=new Scanner(System.in);
+        String s=kb.nextLine();
+        if(s.contains("0000000") || s.contains("1111111")){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
