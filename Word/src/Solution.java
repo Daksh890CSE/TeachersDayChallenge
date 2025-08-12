6007eedfe672e8042a953445b75662d8ba536a90
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-12 19:32:34 +0530

    Initialize variables for upper and lower case character counting in Solution class

--- Word/src/Solution.java
+++ Word/src/Solution.java
@@ -4,6 +4,10 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
-        
+        int upper=0;
+        int lower=0;
+        for(int i=0;i<s.length();i++){
+            
+        }
     }
 }
