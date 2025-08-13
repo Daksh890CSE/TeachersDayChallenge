a3ff47af164419a9a5e7a31e24d72d2c46d54185
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-13 15:57:44 +0530

    Fix loop initialization in Solution class to correctly reset index for consecutive input handling

--- Beautiful Matrix/src/Solution.java
+++ Beautiful Matrix/src/Solution.java
@@ -6,14 +6,14 @@ public class Solution {
         int[] arr=new int[5];
         int row=0;
         int column=0;
-        for(int i=00;i<5;i++){
+        for(int i=0;i<5;i++){
             arr[i]=kb.nextInt();
             if(arr[i]==1){
                 column=i;
                 break;
             }else if(i==4 && arr[4]!=1){
                 row+=1;
-                i=0;
+                i=-1;
             }
         }
         System.out.println(row);
