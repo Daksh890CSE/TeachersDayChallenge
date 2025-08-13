9fc118f582a127c0c94f6e9f94c304d70b54e25f
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-13 09:05:27 +0530

    Initialize project structure with settings, README, and basic Solution class implementation

---
+++ Beautiful Matrix/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        int[][] arr=new int[5][5];
+        for(int i=0;i<5;i++)
+    } 
+}
