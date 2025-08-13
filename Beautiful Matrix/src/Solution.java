d49f35294e7639ee5ffd88d51a0dcc8e3d3b4d30
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-13 15:45:32 +0530

    Refactor Solution class to implement logic for identifying row and column of first '1' in a 1D array

--- Beautiful Matrix/src/Solution.java
+++ Beautiful Matrix/src/Solution.java
@@ -3,7 +3,20 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
-        int[][] arr=new int[5][5];
-        for(int i=0;i<5;i++)
+        int[] arr=new int[5];
+        int row=0;
+        int column=0;
+        for(int i=00;i<5;i++){
+            arr[i]=kb.nextInt();
+            if(arr[i]==1){
+                column=i;
+                break;
+            }else if(i==4 && arr[4]!=1){
+                row+=1;
+                i=0;
+            }
+        }
+        System.out.println(row);
+        System.out.println(column);
     } 
 }
