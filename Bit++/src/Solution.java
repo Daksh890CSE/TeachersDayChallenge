5e6be71945f565d923a54a6dc92394f473586e78
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-14 21:57:09 +0530

    Implement input handling and loop structure in Solution class

--- Bit++/src/Solution.java
+++ Bit++/src/Solution.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args){
-        
+        Scanner kb=new Scanner(System.in);
+        int x=0;
+        int n=kb.nextInt();
+        kb.nextLine();
+        for(int i=0;n<i;i++){
+            String s=kb.nextLine();
+        }
     }
 }
