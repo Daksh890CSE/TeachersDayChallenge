e9cc9830e405fbf8af0365d2390875dbf96f82e7
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-15 18:25:42 +0530

    Add input handling and area calculation in main method of Solution class

--- Domino piling/src/Solution.java
+++ Domino piling/src/Solution.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
+        int m=kb.nextInt();
+        int n=kb.nextInt();
+        int area=m*n;
+        
     }
 }
