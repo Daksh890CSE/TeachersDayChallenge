057085f3f278059c7ea531e32b3b75ed29cf80f6
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-15 19:13:29 +0530

    Add count variable initialization in Solution class

--- Twins/src/Solution.java
+++ Twins/src/Solution.java
@@ -6,10 +6,12 @@ public class Solution {
         int n = kb.nextInt();
         int[] arr = new int[n];
         int sum = 0;
+        int count=0;
         for (int i = 0; i < n; i++) {
             arr[i] = kb.nextInt();
             sum+=arr[i];
         }
         Arrays.sort(arr);
+        for(int i=0; )
     }
 }
