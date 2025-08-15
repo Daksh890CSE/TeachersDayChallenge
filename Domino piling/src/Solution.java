795c88109352ee274da490233e81854fb3388b02
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-15 18:25:59 +0530

    Ensure Scanner resource is closed after reading input in Solution class

--- Domino piling/src/Solution.java
+++ Domino piling/src/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
         Scanner kb=new Scanner(System.in);
         int m=kb.nextInt();
         int n=kb.nextInt();
+        kb.close();
         int area=m*n;
         
     }
