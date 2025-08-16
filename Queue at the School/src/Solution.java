78ab96f1be5c880fd2016119803739c68548dded
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-16 22:27:06 +0530

    Refactor main method for improved readability and structure in Solution class

--- Queue at the School/src/Solution.java
+++ Queue at the School/src/Solution.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args){
-        Scanner kb=new Scanner(System.in);
-        int n=kb.nextInt();
-        int t=kb.nextInt();
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int t = kb.nextInt();
         kb.nextLine();
-        char[] ch=kb.nextLine().toCharArray();
+        char[] ch = kb.nextLine().toCharArray();
         kb.close();
-        for(int i=1;i<n;i++){
-            if(ch[i-1]=='B' && ch[i]=='G'){
-                ch[i-1]='G';
-                ch[i]='B';
-                if(i%t==0){
-                    i++;
+        for (int i = 0; i < t; i++) {
+            for (int j = 1; j < n; j++) {
+                if (ch[j - 1] == 'B' && ch[j] == 'G') {
+                    ch[j - 1] = 'G';
+                    ch[j] = 'B';
+                    j++;
                 }
             }
         }
-        String s=new String(ch);
+        String s = new String(ch);
         System.out.println(s);
     }
 }
