1a24b9496918e0bb6f93b32fddba964d90171918
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-16 11:51:32 +0530

    Add initial project structure and Solution class implementation

---
+++ Queue at the School/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        int t=kb.nextInt();
+        kb.nextLine();
+        String s=kb.nextLine();
+        for(int i=0;i<s.length();i++)
+    }
+}
