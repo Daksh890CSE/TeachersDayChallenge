11f151b25ad4f20e35ff76c9432733a77641429b
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-24 21:59:49 +0530

    Add initial project structure with settings, README, and Solution class

---
+++ Soldier and Bananas/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public static int borrow(int price,int money,int banana){
+
+    }
+    public static void main(String[] args){
+        
+    }
+}
