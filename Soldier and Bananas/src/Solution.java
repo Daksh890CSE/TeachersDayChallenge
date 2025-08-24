a9a2bfa4118f10f4c3311e7a285c9c503db2cc4f
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-24 22:08:32 +0530

    Refactor borrow method to initialize cost and iterate over bananas

--- Soldier and Bananas/src/Solution.java
+++ Soldier and Bananas/src/Solution.java
@@ -2,12 +2,17 @@ import java.util.*;
 
 public class Solution {
     public static int borrow(int price,int money,int banana){
-
+        int cost=price;
+        for(int i=1;i<banana;i++){
+            
+        }
     }
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int price=kb.nextInt();
         int money=kb.nextInt();
         int banana=kb.nextInt();
+        kb.close();
+        System.out.println(borrow(price,money,banana));
     }
 }
