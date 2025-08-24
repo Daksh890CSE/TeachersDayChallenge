e773b1cd082f5d69752f1e0f87408838a27571fc
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-24 20:47:37 +0530

    Refactor minSteps method to improve logic and readability

--- Elephant/src/Solution.java
+++ Elephant/src/Solution.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class Solution {
     public static int minSteps(int x){
+        int count= 1;
         if(x<5){
             return 1;
+        }else{
+            return (x/5+1);
         }
     }
     public static void main(String[] args){
