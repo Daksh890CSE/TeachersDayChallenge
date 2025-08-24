59941f08de1c1f93a4037d92708e621586f4ea8d
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-24 22:02:05 +0530

    Implement input handling in main method for price, money, and banana

--- Soldier and Bananas/src/Solution.java
+++ Soldier and Bananas/src/Solution.java
@@ -5,6 +5,9 @@ public class Solution {
 
     }
     public static void main(String[] args){
-        
+        Scanner kb=new Scanner(System.in);
+        int price=kb.nextInt();
+        int money=kb.nextInt();
+        int banana=kb.nextInt();
     }
 }
