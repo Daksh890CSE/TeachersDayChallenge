6832980aa37930e42cb6d28b1b94c5b0193d892e
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-24 23:06:14 +0530

    Refactor borrow method to replace while loop with for loop for cost calculation

--- Soldier and Bananas/src/Solution.java
+++ Soldier and Bananas/src/Solution.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class Solution {
     public static int borrow(int price,int money,int banana){
-        while(banana!=1){
-            price+=price*2;
-            banana--;
+        int cost=0;
+        for(int i=1;i<=banana;i++){
+            cost+=price*i;
+            System.out.println(cost);
         }
-        System.out.println(price);
+        return cost-money;
     }
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
