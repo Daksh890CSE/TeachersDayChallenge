65892e3b9d41cca41af12732e7e96d3ccde7fe6d
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 14:58:28 +0530

    Add initial implementation of Solution class with user input handling

--- Drinks/src/Solution.java
+++ Drinks/src/Solution.java
@@ -3,5 +3,10 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        float result;
+        for(int i=0;i<n;i++){
+            
+        }
     }
 }
