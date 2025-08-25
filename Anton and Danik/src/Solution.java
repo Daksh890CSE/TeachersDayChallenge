62145a05db44cab2f721f207d450781759013744
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 18:38:36 +0530

    Implement initial structure for Solution class with input handling and loop setup

--- Anton and Danik/src/Solution.java
+++ Anton and Danik/src/Solution.java
@@ -3,5 +3,16 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        kb.nextLine();
+        String s=kb.nextLine();
+        kb.close();
+        int countA=0;
+        int countD=0;
+        for(int i=0;i<n;i++){
+            switch case{
+                
+            }
+        }
     }
 }
