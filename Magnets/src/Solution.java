6681870eaf81365d45bda04eddc3f4bbed0bac71
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 19:31:48 +0530

    Fix initial group count and update condition for character grouping in Solution class

--- Magnets/src/Solution.java
+++ Magnets/src/Solution.java
@@ -4,14 +4,14 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        int group=0;
+        int group=1;
         String arr[]=new String[n];
         for(int i=0;i<n;i++){
             arr[i]=kb.nextLine();
         }
         for(int i=1;i<n;i++){
-            if(arr[i-1]=="01" && arr[i]=="10"){
-
+            if(arr[i-1]=="01" && arr[i]=="10" || arr[i-1]=="10" && arr[i]=="01"){
+                group++;
             }
         }
     }
