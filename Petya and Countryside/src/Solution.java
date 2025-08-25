7e066ef328529cbcbb4b7002c2693ff9bc03f878
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 17:07:34 +0530

    Add initial implementation of Solution class and project settings

---
+++ Petya and Countryside/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[i]=kb.nextInt();
+        }
+
+        for(int i=0;i<n;i++)
+    }
+}
