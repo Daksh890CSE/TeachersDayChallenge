8bb35e417ac2128876cea24fdedeaa9e670b0983
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 15:17:27 +0530

    Refactor main method to use double for fractional calculations and streamline input handling

--- Drinks/src/Solution.java
+++ Drinks/src/Solution.java
@@ -4,9 +4,10 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        float result;
+        double frac=0;
         for(int i=0;i<n;i++){
-            
+            frac+=kb.nextInt()/100;
         }
+        System.out.println(frac/n*100);
     }
 }
