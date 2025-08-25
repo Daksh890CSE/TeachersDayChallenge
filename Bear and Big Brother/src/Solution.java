b0defea765488b4b2f3aa4762cf28a8fe662c85f
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 18:52:29 +0530

    Implement input handling for character counting in Solution class

--- Bear and Big Brother/src/Solution.java
+++ Bear and Big Brother/src/Solution.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args){
-        
+        Scanner kb=new Scanner(System.in);
+        int a=kb.nextInt();
+        int b=kb.nextInt();
+        int year=0;
+        while(a<b)
     }
 }
