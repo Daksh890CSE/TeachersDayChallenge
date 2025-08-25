2727fd59fd28fdb908349727d71adf7d58fff204
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 20:35:00 +0530

    Fix string comparison logic and improve input handling in Solution class

--- Magnets/src/Solution.java
+++ Magnets/src/Solution.java
@@ -4,15 +4,18 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
+        kb.nextLine();
         int group=1;
         String arr[]=new String[n];
         for(int i=0;i<n;i++){
             arr[i]=kb.nextLine();
         }
+        kb.close();
         for(int i=1;i<n;i++){
-            if(arr[i-1]=="01" && arr[i]=="10" || arr[i-1]=="10" && arr[i]=="01"){
+            if(arr[i-1].equals("01") && arr[i].equals("10") || arr[i-1].equals("10") && arr[i].equals("01")){
                 group++;
             }
         }
+        System.out.println(group);
     }
 }
