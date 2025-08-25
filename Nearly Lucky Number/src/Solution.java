6eee3401c37435c71b01b9bf0442b28e179f6f6c
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 21:12:04 +0530

    Implement input handling for the main method in Solution class

--- Nearly Lucky Number/src/Solution.java
+++ Nearly Lucky Number/src/Solution.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args){
-        
+        Scanner kb=new Scanner(System.in);
+        long n=kb.nextLong();
+        String s=String.valueOf(n);
+        if
     }
 }
