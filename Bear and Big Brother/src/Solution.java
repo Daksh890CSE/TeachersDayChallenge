7c3532344b41518c59555f3560f8f2b3dff2e47e
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-25 19:17:32 +0530

    Fix loop condition and update multiplication logic in Solution class

--- Bear and Big Brother/src/Solution.java
+++ Bear and Big Brother/src/Solution.java
@@ -7,9 +7,9 @@ public class Solution {
         int b=kb.nextInt();
         kb.close();
         int year=0;
-        while(a<b){
-            a+=a*3;
-            b+=b*2;
+        while(a<=b){
+            a=a*3;
+            b=b*2;
             year++;
         }
         System.out.println(year);
