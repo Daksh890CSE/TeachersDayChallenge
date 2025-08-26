5add2e003c965586fb7a3ea34e5e444823171322
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-26 14:55:55 +0530

    Initialize boolean array and set default values in Solution class

--- Panoramix's Prediction/src/Solution.java
+++ Panoramix's Prediction/src/Solution.java
@@ -5,5 +5,11 @@ public class Solution {
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
         int m=kb.nextInt();
+        boolean[] bool=new boolean[50];
+        for(int i=0;i<50;i++){
+            bool[i]=true;
+        }for(int i=2;i<Math.sqrt(50);i++){
+            
+        }
     }
 }
