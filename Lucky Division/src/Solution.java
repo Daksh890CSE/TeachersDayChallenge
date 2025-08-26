4d5208cedc0c58cfeb5912f09ba8dc3f92bf72bb
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-26 16:08:25 +0530

    Fix logic in Solution class to check for lucky numbers and handle input correctly

--- Lucky Division/src/Solution.java
+++ Lucky Division/src/Solution.java
@@ -4,6 +4,11 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        
+        String s=String.valueOf(n);
+        if(n%4==0 || n%7==0){
+            System.out.println("YES");
+        }else if(s.contains("4") || s.contains("7")){
+            s=s.replaceAll("^[47]+$", s)
+        }
     }
 }
