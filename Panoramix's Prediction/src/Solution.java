143fffa73b92a817ceeb41f2de1ddc78a4df33ac
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-26 15:09:57 +0530

    Refactor main method in Solution class for improved readability and structure

--- Panoramix's Prediction/src/Solution.java
+++ Panoramix's Prediction/src/Solution.java
@@ -1,15 +1,25 @@
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args){
-        Scanner kb=new Scanner(System.in);
-        int n=kb.nextInt();
-        int m=kb.nextInt();
-        boolean[] bool=new boolean[50];
-        for(int i=0;i<50;i++){
-            bool[i]=true;
-        }for(int i=2;i<Math.sqrt(50);i++){
-            
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        boolean[] bool = new boolean[50];
+        for (int i = 0; i < 50; i++) {
+            bool[i] = true;
+        }
+        for (int i = 2; i < Math.sqrt(50); i++) {
+            if (bool[i] == true) {
+                for (int j = i * i; j < 50; j += i) {
+                    bool[j] = false;
+                }
+            }
+        }
+        for (int i = 3; i < 50; i++) {
+            if (bool[i] == true) {
+                System.out.println(i);
+            }
         }
     }
 }
