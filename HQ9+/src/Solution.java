205cdb09d135b647759c2936fc35826b55a669a9
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-31 21:14:18 +0530

    Add initial project setup with settings and README; implement Solution class for lucky number checks

---
+++ HQ9+/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public static void isTrue(){
+
+    }
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        
+    }
+}
--- Lucky Division/src/Solution.java
+++ Lucky Division/src/Solution.java
@@ -4,11 +4,19 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
+        kb.close();
         String s=String.valueOf(n);
-        if(n%4==0 || n%7==0){
+        if(n%4==0 || n%7==0 || n%47==0){
             System.out.println("YES");
         }else if(s.contains("4") || s.contains("7")){
-            s=s.replaceAll("^[47]+$", s)
+            s=s.replaceAll("^[47]+$", "");
+            if(s.isBlank()){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }else{
+            System.out.println("NO");
         }
     }
 }
