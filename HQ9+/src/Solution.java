c6eee2a1202ead4379d0f9ec9204dd1389949a70
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-08-31 21:21:50 +0530

    Implement main logic in Solution class to check for HQ9+ based on user input

--- HQ9+/src/Solution.java
+++ HQ9+/src/Solution.java
@@ -1,12 +1,20 @@
 import java.util.*;
 
 public class Solution {
-    public static void isTrue(){
-
-    }
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
         kb.close();
+        if(s.contains("H")){
+            System.out.println("YES");
+        }else if(s.contains("Q")){
+            System.out.println("YES");
+        }else if(s.contains("9")){
+            System.out.println("YES");
+        }else if(s.contains("+")){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
