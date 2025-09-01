0f542a33e76dac54bf946cb06f7772d619fa53c1
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-01 17:43:14 +0530

    Add missing logic in function and print result in main method

--- Calculating Function/src/Solution.java
+++ Calculating Function/src/Solution.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class Solution {
     public static long function(long n){
-
+        int b=n-1
+        if(n%2==0)
     }
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
         kb.close();
+        System.out.println(function(n));
     }
 }
