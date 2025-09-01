16614a9c433e99c0a1a34218c9135a87ad2530b5
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-01 19:06:08 +0530

    Refactor main method to correctly check for uppercase and lowercase characters

--- cAPS lOCK/src/Solution.java
+++ cAPS lOCK/src/Solution.java
@@ -4,8 +4,15 @@ public class Solution {
     public static void main(String args[]){
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
+        boolean istrue=false;
         for(int i=0;i<s.length();i++){
-            
+            if(Character.isUpperCase(s.charAt(i))){
+                istrue=true;
+            }else if(Character.isLowerCase(0)){
+                istrue=true;
+            }else{
+                istrue=false;
+            }
         }
     }
 }
