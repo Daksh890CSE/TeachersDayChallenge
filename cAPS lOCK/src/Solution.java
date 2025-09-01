29fb6153b386463f16f06dc95d8816936c93dff8
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-01 19:49:45 +0530

    Refactor main method to improve character case handling and streamline output logic

--- cAPS lOCK/src/Solution.java
+++ cAPS lOCK/src/Solution.java
@@ -5,19 +5,24 @@ public class Solution {
         Scanner kb=new Scanner(System.in);
         String s=kb.nextLine();
         kb.close();
+        int count=0;
         boolean istrue=false;
         for(int i=0;i<s.length();i++){
             if(Character.isUpperCase(s.charAt(i))){
                 istrue=true;
+                count++;
             }else if(Character.isLowerCase(s.charAt(0)) && i==0){
                 istrue=true;
             }else{
                 istrue=false;
                 break;
             }
-        }if(istrue==true){
+        }if(istrue==true && count!=s.length()){
             s=s.toLowerCase();
             s=s.replaceFirst(String.valueOf(s.charAt(0)), String.valueOf(Character.toUpperCase(s.charAt(0))));
+            System.out.println(s);
+        }else if(istrue==true && count==s.length()){
+            s=s.toLowerCase();
             System.out.println(s);
         }else{
             System.out.println(s);
