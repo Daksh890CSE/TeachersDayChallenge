62661033d02c4c758d0aca5521ffe7e3474d5aaa
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-01 18:24:32 +0530

    Implement input reading and loop through string characters in main method

--- cAPS lOCK/src/Solution.java
+++ cAPS lOCK/src/Solution.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class Solution {
     public static void main(String args[]){
         Scanner kb=new Scanner(System.in);
-        
+        String s=kb.nextLine();
+        for(int i=0;i<s.length();i++){
+            
+        }
     }
 }
