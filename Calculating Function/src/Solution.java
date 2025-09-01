bea171681fe44c433d68465f9f8a80c03d9686cd
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-01 17:41:46 +0530

    Update function signature to use long type for parameter in Solution class

--- Calculating Function/src/Solution.java
+++ Calculating Function/src/Solution.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class Solution {
-    public static int function(int n){
-        
+    public static long function(long n){
+
     }
     public static void main(String[] args){
-
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        
     }
 }
