467a4bda7d2a6eb13c9c1fd68186bfcc8889462a
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-02 19:17:08 +0530

    Implement main logic to read input and determine output based on sums of three variables

--- Young Physicist/src/Solution.java
+++ Young Physicist/src/Solution.java
@@ -3,5 +3,26 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        int x=0;
+        int y=0;
+        int z=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<4;i++){
+                if(j==1){
+                    x+=kb.nextInt();
+                }else if(j==2){
+                    y+=kb.nextInt();
+                }else{
+                    z+=kb.nextInt();
+                }
+            }
+        }
+        kb.close();
+        if(x==0 & y==0 & z==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
