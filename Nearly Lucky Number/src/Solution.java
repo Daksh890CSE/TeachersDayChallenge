451872f662e732709fc770a5cdabf77054265326
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-02 21:50:43 +0530

    Refactor output logic and remove unnecessary print statement in Solution class

--- Nearly Lucky Number/src/Solution.java
+++ Nearly Lucky Number/src/Solution.java
@@ -4,13 +4,19 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         long n=kb.nextLong();
+        kb.close();
         int count=0;
         while(n!=0){
             long a=n%10;
             if(a==7 || a==4){
                 count++;
             }
+            n/=10;
         }
-        System.out.println(count);
+        if(count==7 || count==4){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
