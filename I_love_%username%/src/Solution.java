ec1993c1c372f60e4b00fafd073d37a892143980
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-02 21:24:45 +0530

    Refactor max and min initialization and simplify loop conditions for clarity

--- I_love_%username%/src/Solution.java
+++ I_love_%username%/src/Solution.java
@@ -5,23 +5,21 @@ public class Solution {
         Scanner kb=new Scanner(System.in);
         int n = kb.nextInt();
         int[] score=new int[n];
-        int max=-1;
-        int min=10001;
         int count=0;
         for(int i=0;i<n;i++){
             score[i]=kb.nextInt();
         }
+        int max=score[0];
+        int min=score[0];
         for(int i=1;i<n;i++){
-            if(score[i-1]<score[i]){
-                if(max<score[i]){
-                    max=score[i];
-                    count++;
-                }else{
-
-                }
+            if(max<score[i]){
+                max=score[i];
+                count++;
+            }if(min>score[i]){
+                min=score[i];
+                count++;
             }
-        }
-        
+        }    
         System.out.println(count);
         kb.close();
     }
