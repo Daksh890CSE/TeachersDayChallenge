b74e37eac4453c1297c32f3c49f6f416d8258481
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-02 21:47:16 +0530

    Refactor input handling and counting logic for lucky numbers

--- Nearly Lucky Number/src/Solution.java
+++ Nearly Lucky Number/src/Solution.java
@@ -4,15 +4,13 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         long n=kb.nextLong();
-        String s=String.valueOf(n);
-        kb.close();
-        if(s.contains("4")&&s.contains("7")){
-             s=s.replaceAll("^[47]+$", "");
+        int count=0;
+        while(n!=0){
+            long a=n%10;
+            if(a==7 || a==4){
+                count++;
+            }
         }
-        if(s.isEmpty()){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
+        System.out.println(count);
     }
 }
