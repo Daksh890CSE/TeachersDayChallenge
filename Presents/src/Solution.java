5cb7138ed12491c911ea48f64e491983453bc5d7
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-03 19:40:54 +0530

    Add input handling for presents in Solution class

--- Presents/src/Solution.java
+++ Presents/src/Solution.java
@@ -3,5 +3,10 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] present=new int[n];
+        for(int i=0;i<n;i++){
+            present[i]=kb.nextInt();
+        }
     }
 }
