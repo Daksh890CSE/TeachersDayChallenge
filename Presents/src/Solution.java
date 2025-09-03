d372d12a3051cc94faf4dd2d4ac2d54fa2e13b3f
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-03 19:41:10 +0530

    Add logic to populate final array with adjusted present indices

--- Presents/src/Solution.java
+++ Presents/src/Solution.java
@@ -5,8 +5,12 @@ public class Solution {
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
         int[] present=new int[n];
+        int[] fnl=new int[n];
         for(int i=0;i<n;i++){
             present[i]=kb.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            fnl[present[i]-1]=present[i]-1;
         }
     }
 }
