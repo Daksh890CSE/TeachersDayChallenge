027efd2e3244ffb26fb09044638e8d1366a0cab5
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-04 15:50:29 +0530

    Refactor array initialization to use a primitive array instead of ArrayList in Solution class

--- Arrival of the General/src/Solution.java
+++ Arrival of the General/src/Solution.java
@@ -4,10 +4,9 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        ArrayList<Integer> arr=new ArrayList<>(n);
+        int[] arr=new int[n];
         for(int i=0;i<n;i++){
-            arr.add(kb.nextInt());
+            arr[i]=kb.nextInt();
         }
-        
     }
 }
