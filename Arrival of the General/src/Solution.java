a90ef341f7da7c90e96b7f5cefe3337be54a7d47
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-04 15:38:05 +0530

    Optimize array initialization by specifying initial capacity in Solution class

--- Arrival of the General/src/Solution.java
+++ Arrival of the General/src/Solution.java
@@ -4,7 +4,10 @@ public class Solution {
     public static void main(String[] args){
         Scanner kb=new Scanner(System.in);
         int n=kb.nextInt();
-        ArrayList<Integer> arr=new ArrayList<>();
-
+        ArrayList<Integer> arr=new ArrayList<>(n);
+        for(int i=0;i<n;i++){
+            arr.add(kb.nextInt());
+        }
+        
     }
 }
