85878f105f1e36fe57b73fece98b4de0af9724f2
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-09-04 15:34:50 +0530

    Initialize project structure with settings, README, and main solution class

---
+++ Arrival of the General/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        ArrayList<Integer> arr=new ArrayList<>();
+
+    }
+}
